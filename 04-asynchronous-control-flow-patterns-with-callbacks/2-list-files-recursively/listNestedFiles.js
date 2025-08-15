64053df2df58fae325ef8339e8d89f0db067cd22
Pavel Karyapkin <karyapkin@gmail.com>
2025-08-15 16:48:57 +0500

    add listNestedFiles

---
+++ 04-asynchronous-control-flow-patterns-with-callbacks/2-list-files-recursively/listNestedFiles.js
@@ -0,0 +1,23 @@
+import fs from 'node:fs';
+
+function listNestedFiles(dir, cb) {
+	fs.readdir(dir, { recursive: true }, (err, pathes) => {
+		if (err) return cb(err);
+		const files = [];
+		pathes.forEach((path, index) => {
+			fs.stat(dir + path, (err, stats) => {
+				if (err) return cb(err);
+				if (!stats.isDirectory()) files.push(path);
+				if (pathes.length - 1 === index) cb(null, files);
+			});
+		});
+	});
+}
+
+function finish(err, res) {
+	if (err) return console.error(err);
+	console.log(res, 'have been found');
+}
+
+listNestedFiles(process.argv[2], finish);
+
